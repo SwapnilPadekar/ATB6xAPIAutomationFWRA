d683f37f109a6211b25858232146e5fef7c5e843
SwapnilPadekar <slpadekar29@gmail.com>
2024-07-02 22:16:01 +0530

    added files to FW

---
+++ src/main/java/com/thetestingacademy/endpoints/APIConstants.java
@@ -0,0 +1,14 @@
+package com.thetestingacademy.endpoints;
+
+public class APIConstants {
+
+    //static is used because we can instantiate without using class
+
+    public static String BASE_URL = "https://restful-booker.herokuapp.com";
+    public static String CREATE_UPDATE_BOOKING_URL = "/booking";
+    public static String AUTH_URL = "/auth";
+    public static String PING_URL = "/ping";
+
+
+
+}
---
+++ src/test/java/com/thetestingacademy/actions/AssertActions.java
@@ -0,0 +1,45 @@
+package com.thetestingacademy.actions;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import io.restassured.response.Response;
+
+
+public class AssertActions {
+
+    public void verifyResponseBody(String actual, String expected, String description){
+        assertEquals(actual, expected, description);
+    }
+
+    //method overloading
+    public void verifyResponseBody(int actual, int expected, String description){
+        assertEquals(actual, expected, description);
+    }
+
+    //method overloading
+    public void verifyResponseBody(float actual, float expected, String description){
+        assertEquals(actual, expected, description);
+    }
+
+    //method overloading
+    public void verifyResponseBody(double actual, double expected, String description) {
+        assertEquals(actual, expected, description);
+
+    }
+
+    //method overloading
+    public void verifyResponseBody(boolean actual, boolean expected, String description) {
+        assertEquals(actual, expected, description);
+    }
+
+    public void verifyStatusCodeInvalidReq(Response response) {
+        assertTrue(String.valueOf(response.getStatusCode()).startsWith("50"),
+                "value of status code is" + response.getStatusCode());
+    }
+
+    public void verifyStatusCode(Response response,Integer expected) {
+        assertEquals(response.getStatusCode(),expected);
+    }
+
+
+}
